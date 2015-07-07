84bfdd35003653ff79713f2f5b8da0a81341a28a
Tomi Novak <tominovak33@gmail.com>
2015-07-07 21:05:16 +0100

    add config example

---
+++ includes/config.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomi
+ * Date: 07/07/15
+ * Time: 20:17
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'username-here');
+define('DB_PASSWORD', 'password-here');
+define('DB_NAME', 'pattern_db');
