55dfacfd3af111ca95f6941a63c7c4c5b2610b83
Tomi Novak <tominovak33@gmail.com>
2015-07-07 22:47:05 +0100

    database code refactor

--- includes/config.example.php
+++ includes/config.example.php
@@ -10,3 +10,8 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'username-here');
 define('DB_PASSWORD', 'password-here');
 define('DB_NAME', 'pattern_db');
+
+define('FRONTEND_DOMAIN', 'http://frontend-lapis.local.dev');
+
+//Allow C.O.R.S. between API and frontend application
+header("Access-Control-Allow-Origin: FRONTEND_DOMAIN");
--- includes/database.php
+++ includes/database.php
@@ -12,76 +12,89 @@ class Database {
         //nothing yet - maybe put code from connect() here later
     }
 
-    public function connect () {
+    static $_connection;
 
-        $database_connection=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    public static function connect () {
 
-        if (mysqli_errno($database_connection)) {
-            die("Failed to connect to database: " . mysqli_connect_error($database_connection));
+
+        if (!self::$_connection) {
+            self::$_connection = new mysqli(DB_HOST,DB_USER,DB_PASSWORD, DB_NAME);
+        }
+        if (!self::$_connection) {
+            trigger_error('DB ERROR connecting to database: '.mysqli_connect_error(), E_USER_ERROR);
+        }
+        if (mysqli_connect_errno(self::$_connection)) {
+            trigger_error('DB ERROR selecting database: '.mysqli_connect_error(), E_USER_ERROR);
         }
 
-        return $database_connection;
+        return self::$_connection;
     }
 
-    public function database_query($database_connection, $sql_query) {
-        $query_result=mysqli_query($database_connection, $sql_query);
+    public static function query($sql_query) {
+        $_connection = self::connect();
+
+        $query_result=$_connection->query($sql_query);
         return $query_result;
     }
 
-    /**
-     * get the query column by its ID
-     * if $column is set to (bool)true then entire row is returned
-     */
-    public function query_row_by_id($query_result, $column) {
+}
 
-        $result_row=mysqli_fetch_row($query_result);
-        if ($column === true) {
-            return $result_row;
-        }
+function database_query($sql_query) {
+    return Database::query($sql_query);
+}
 
-        if (isset($result_row[$column])) {
-            return $result_row[$column];
-        }
-        else {
-            return false;
-        }
+
+/**
+ * get the query column by its ID
+ * if $column is set to (bool)true then entire row is returned
+ */
+function query_row_by_id($query_result, $column) {
+
+    $result_row=mysqli_fetch_row($query_result);
+    if ($column === true) {
+        return $result_row;
     }
 
-    /**
-     * get the query column by its name
-     * if $column is set to (bool)true then entire row is returned
-     */
-    public function query_row_by_name($query_result, $column) {
-        $result_row=mysqli_fetch_assoc($query_result);
-        if ($column === true) {
-            return $result_row;
-        }
+    if (isset($result_row[$column])) {
+        return $result_row[$column];
+    }
+    else {
+        return false;
+    }
+}
 
-        if (isset($result_row[$column])) {
-            return $result_row[$column];
-        }
-        else {
-            return false;
-        }
+/**
+ * get the query column by its name
+ * if $column is set to (bool)true then entire row is returned
+ */
+function query_row_by_name($query_result, $column) {
+    $result_row=mysqli_fetch_assoc($query_result);
+    if ($column === true) {
+        return $result_row;
     }
 
-    public function db_number_of_rows($query_result) {
-        $rows=mysqli_num_rows($query_result);
-        return $rows;
+    if (isset($result_row[$column])) {
+        return $result_row[$column];
     }
-
-    public function db_escape_string($database_connection, $string) {
-        $escaped_string=mysqli_real_escape_string($database_connection, $string);
-        return $escaped_string;
+    else {
+        return false;
     }
+}
 
-    public function db_affected_rows($database_connection) {
-        return mysqli_affected_rows($database_connection);
-    }
+function db_number_of_rows($query_result) {
+    $rows=mysqli_num_rows($query_result);
+    return $rows;
+}
 
-    public function db_returned_rows($query_result){
-        return mysqli_num_rows($query_result);
-    }
+function db_escape_string($string) {
+    $escaped_string=mysqli_real_escape_string(Database::connect(), $string);
+    return $escaped_string;
+}
 
+function db_affected_rows() {
+    return mysqli_affected_rows(Database::connect());
+}
 
+function db_returned_rows($query_result){
+    return mysqli_num_rows($query_result);
 }
