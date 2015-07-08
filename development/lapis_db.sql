d7992543c65e1409f9a6dcf063e4dc6bfc205f8a
Tomi Novak <tominovak33@gmail.com>
2015-07-08 23:46:18 +0100

    add db structure sql file

---
+++ development/lapis_db.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 07, 2015 at 09:16 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `lapis_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patterns`
+--
+
+CREATE TABLE IF NOT EXISTS `patterns` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `author_id` int(255) NOT NULL,
+  `technology` longtext NOT NULL,
+  `description` longtext NOT NULL,
+  `code` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
