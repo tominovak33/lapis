801897b7684780d36d1f3f59804662678055c778
Tomi Novak <tominovak33@gmail.com>
2015-07-16 22:42:35 +0100

    fix sql order by

--- includes/content/content.php
+++ includes/content/content.php
@@ -69,9 +69,7 @@ class Content {
 
     function add_options_to_query($query) {
         if ($this->get_query_options('ORDER')) {
-            $query .= sprintf("ORDER BY
-            '%s' %s
-            ",
+            $query .= sprintf("ORDER BY patterns.%s %s ",
                 db_escape_string($this->get_query_options('ORDER_BY')),
                 db_escape_string($this->get_query_options('ORDER'))
             );
