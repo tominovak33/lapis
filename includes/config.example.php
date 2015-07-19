12c73a103c82e36970fe7be142f8a9af2b3c6d4e
Tomi Novak <tominovak33@gmail.com>
2015-07-19 21:58:02 +0100

    fix CORS header

--- includes/config.example.php
+++ includes/config.example.php
@@ -25,7 +25,7 @@ define('FRONTEND_DOMAIN', '<frontend_domain>');
 
 
 /*---------------------------
-/ Other Constaints
+/ Other Settings
 /---------------------------*/
 //Allow C.O.R.S. between API and frontend application
-header("Access-Control-Allow-Origin: FRONTEND_DOMAIN");
+header("Access-Control-Allow-Origin:" .FRONTEND_DOMAIN );
