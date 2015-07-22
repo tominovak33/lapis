8c532121def9c32780c25b5271cd3b2676cc65fa
Tomi Novak <tominovak33@gmail.com>
2015-07-22 20:24:23 +0100

    create log file after deploy

--- development/deploy.sh
+++ development/deploy.sh
@@ -31,4 +31,6 @@ rm -rf www # Delete the current web folder
 
 ln -s releases/$DEPLOY_PATH/lapis www # Symlink the new folder that was just cloned
 
+touch www/logs/query_log.txt
+
 echo 'Deployment Complete'
