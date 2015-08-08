1f16fa2040fd44a1abc949c8ff4e27ba752bf350
Tomi Novak <tominovak33@gmail.com>
2015-08-08 23:01:23 +0100

    add initital (blank) user class

---
+++ includes/user.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomi
+ * Date: 06/06/15
+ * Time: 22:30
+ */
+
+class User {
+
+  var $user_id;
+  var $username;
+  var $first_name;
+  var $last_name;
+  var $email;
+  var $password_hash;
+  var $permission_level = 0; // coming soon
+
+  function __construct() {
+    // coming soon
+  }
+
+}
