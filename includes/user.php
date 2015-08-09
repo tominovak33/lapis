8502456be36c0c8c9d67f75ba8ebd48ee11674ac
Tomi Novak <tominovak33@gmail.com>
2015-08-09 21:23:52 +0100

    load user detail functions

--- includes/user.php
+++ includes/user.php
@@ -81,5 +81,13 @@ class User {
     // Populate user properties from the returned array here
   }
 
+  function populate_user_object($user_details) {
+    if(isset($user_details["user_id"])) $this->set_user_id($user_details['user_id']);
+    if(isset($user_details["username"])) $this->set_username($user_details['username']);
+    if(isset($user_details["first_name"])) $this->set_first_name($user_details['first_name']);
+    if(isset($user_details["last_name"])) $this->set_last_name($user_details['last_name']);
+    if(isset($user_details["email"])) $this->set_email($user_details['email']);
+    if(isset($user_details["password"])) $this->set_password_hash($user_details['password']);
+  }
 
 }
